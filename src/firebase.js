c9eb201a89134bbacf323ef581091ef9d0476957
rajshree vats <2006508@kiit.ac.in>
2021-08-10 18:23:18 +0530

    DB update

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import "firebase/firestore"
 import "firebase/storage"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyCCZF2Ryw_vQ43si6I_7oU0nttDNv-UJ64",
+    authDomain: "editx-dev.firebaseapp.com",
+    projectId: "editx-dev",
+    storageBucket: "editx-dev.appspot.com",
+    messagingSenderId: "69483335725",
+    appId: "1:69483335725:web:36f6dd54d5d9f39ba8e8a4"
 })
 
 
